c58a771e46232afe49b2cb04a7ab4dd0ed97194a
ken88ling <ken88ling@gmail.com>
2016-09-04 12:47:36 +1000

    Add project files.

---
+++ RealEstateMvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RealEstateMvc5.Startup))]
+namespace RealEstateMvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
